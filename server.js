0d57417d0b932ba9bdd60485cf957c08fbc045c5
mahia05 <mahiatabassumchowdhury@gmail.com>
2026-05-27 15:24:35 +0600

    server changes

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/api/auth', require('./web/js/routes/authRoutes'));
-app.use('/api/admin', require('./web/js/routes/adminRoutes'));
-app.use('/api/products', require('./web/js/routes/productsRoutes'));
-app.use('/api/cart', require('./web/js/routes/cartRoutes'));
-app.use('/api/orders', require('./web/js/routes/ordersRoutes'));
+app.use('/api/auth', require('./web/Routes/authRoutes'));
+app.use('/api/admin', require('./web/Routes/adminRoutes'));
+app.use('/api/products', require('./web/Routes/productsRoutes'));
+app.use('/api/cart', require('./web/Routes/cartRoutes'));
+app.use('/api/orders', require('./web/Routes/ordersRoutes'));
 app.get('/', (req, res) => {
     res.json({ status: 'Handmade Marketplace API is running', time: new Date() });
 });
