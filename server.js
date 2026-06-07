00deda6c38d65858031097bdacb1d9e28fa7e163
mahia05 <mahiatabassumchowdhury@gmail.com>
2026-06-07 22:48:09 +0600

    server.js directory change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const connectDB = require('./database/db');
+const connectDB = require('./app/database/db');
 
 const app = express();
 connectDB();
@@ -18,11 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/api/auth', require('./web/Routes/authRoutes'));
-app.use('/api/admin', require('./web/Routes/adminRoutes'));
-app.use('/api/products', require('./web/Routes/productsRoutes'));
-app.use('/api/cart', require('./web/Routes/cartRoutes'));
-app.use('/api/orders', require('./web/Routes/ordersRoutes'));
+app.use('/api/auth', require('./app/web/Routes/authRoutes'));
+app.use('/api/admin', require('./app/web/Routes/adminRoutes'));
+app.use('/api/products', require('./app/web/Routes/productsRoutes'));
+app.use('/api/cart', require('./app/web/Routes/cartRoutes'));
+app.use('/api/orders', require('./app/web/Routes/ordersRoutes'));
+
 app.get('/', (req, res) => {
     res.json({ status: 'Handmade Marketplace API is running', time: new Date() });
 });
